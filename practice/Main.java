01fe39a75fc8fadd21a7cdb346a2a821de26abd4
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2025-12-31 23:06:28 +0530

    done

--- practice/Main.java
+++ practice/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+
+    public static void main(String[] args){
+        // this is my first java program
+        /*this
+        is a 
+        multi 
+        line comment
+         */
+        System.out.println("I am Hindhu");
+        System.out.println("nirlep");
+        System.out.println("nirlep is a good boy");
     }
 }
