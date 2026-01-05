09a58eee47a446a93ce070dfa7e9b159bed21338
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-01-05 19:53:41 +0530

    learning

---
+++ practice/Nirlep.java
@@ -0,0 +1,30 @@
+public class Nirlep {
+    public static void main(String[] args){
+        int age =18;
+        System.out.println("the age is " +age);
+        double price=19.99;
+        double gpa =8.0;
+        System.out.println(gpa);
+        System.out.println("$"+price);
+        char grade='A';
+        System.out.println(grade);
+        boolean isStudent=false;
+        System.out.println(isStudent);
+        if(isStudent){
+            System.out.println("you are a student");
+        }
+        else {
+            System.out.println("your are not a student");
+        }
+        String name="Nirlep code";
+        String email="nirlep931@gamil.com";
+        System.out.println(email);
+        System.out.println(name);
+        System.out.println("your choice is:"+name+" " +email+" " +"$"+price);
+        
+
+
+ 
+    }
+
+}
