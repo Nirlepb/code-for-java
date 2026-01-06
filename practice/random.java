43dcb4c23110637859736731814f44c8fd9d8ccd
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-01-06 22:57:56 +0530

    done

---
+++ practice/random.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        Random r=new Random();
+        int number;
+        number=r.nextInt(1,10);
+        System.out.println(number);
+        
+    }
+
+}
