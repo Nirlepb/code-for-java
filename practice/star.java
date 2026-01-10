edf4e3aa371f172a80763ef8685ee4ee2859614e
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-01-10 14:46:03 +0530

    done

--- practice/star.java
+++ practice/star.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
-public class star {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.print("enter the number of stars: ");
-        int n;
-        n=s.nextInt();
-        for (int i=n; i!=0;i--){
-            for (int j=i;j!=0; j--){
-                 System.out.print("*");
-            }
-        System.out.println();
-        }
-        s.close();
+// public class star {
+//     public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         System.out.print("enter the number of stars: ");
+//         int n;
+//         n=s.nextInt();
+//         for (int i=n; i!=0;i--){
+//             for (int j=i;j!=0; j--){
+//                  System.out.print("*");
+//             }
+//         System.out.println();
+//         }
+//         s.close();
 
 
         
-    }
+//     }
 
-}
+// }
+// public class star{
+//     public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         System.out.print("Enter the number for stars: ");
+//         int n;
+//         n=s.nextInt();
+//         for (int j=1;j<=n;j++){
+//             for (int i=1; i<=j;i++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         s.close();
+//     }
+
+// }
