12b1cf1eb995987df03a58a318858ef6ad21371b
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-01-10 14:30:09 +0530

    done

---
+++ practice/star.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class star {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("enter the number of stars: ");
+        int n;
+        n=s.nextInt();
+        for (int i=n; i!=0;i--){
+            for (int j=i;j!=0; j--){
+                 System.out.print("*");
+            }
+        System.out.println();
+        }
+        s.close();
+
+
+        
+    }
+
+}
