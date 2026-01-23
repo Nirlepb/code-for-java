294df846db3fd49294d6f10368b0f8d8a063ac9e
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-01-23 20:18:42 +0530

    done

---
+++ practice/Swap2Integers.java
@@ -0,0 +1,13 @@
+public class Swap2Integers {
+    public static void main(String[] args) {
+        int a=9;
+        int b=-9;
+        int c=a;
+        a=b;
+        b=c;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
