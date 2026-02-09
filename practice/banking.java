22b42723fe31bb86343b79cabd46c8b19285bbb0
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2026-02-09 23:20:15 +0530

    done

---
+++ practice/banking.java
@@ -0,0 +1,12 @@
+public class banking {
+    public static void main(String[] args) {
+        int x=3;
+        switch (x) {
+            case 2->System.out.println("i am happy");
+        
+            default -> System.out.println("i am sad");
+            
+        }
+    }
+
+}
